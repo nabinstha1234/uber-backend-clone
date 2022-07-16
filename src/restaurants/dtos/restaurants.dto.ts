22f8b98ad3d7bf04070a91efc92ea03d74277dc3
Nabin Shrestha <shresthanabin94@gmail.com>
2022-07-16 11:11:59 +0545

    added validation pipeline

--- src/restaurants/dtos/restaurants.dto.ts
+++ src/restaurants/dtos/restaurants.dto.ts
@@ -1,13 +1,20 @@
 import { Field, ArgsType } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 @ArgsType()
 export class CreateRestaurantDto {
   @Field(() => String)
+  @IsString()
+  @Length(3, 255)
   name: string;
   @Field(() => Boolean)
+  @IsBoolean()
   isVegan: boolean;
   @Field(() => String)
+  @IsString()
   address: string;
   @Field(() => String)
+  @IsString()
+  @Length(3, 255)
   ownerName: string;
 }
